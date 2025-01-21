d82be4003fd9c1476d0455a72f714da997904526
Kazuki-723 <tomita.kazuki.r3@dc.tohoku.ac.jp>
2025-01-22 02:15:20 +0900

    provisional version

--- particle_simulation/particle_simulation.py
+++ particle_simulation/particle_simulation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
 
 # 初期化
 
@@ -17,6 +18,8 @@ X_mesh, Y_mesh = np.meshgrid(x, y)
 N = 1000  # 粒子数
 T = 2300  # 温度 (K)
 M = 29 #分子量
+initialvalue = 100 #初期の粒子数
+time_steps = 10 #時間発展数
 
 #流入速度
 u_x = -500
@@ -38,6 +41,9 @@ speed_sys_y_list = np.zeros((width, height))
 
 #拡散方向リスト 順に+x,+y,-x,-y
 spread =  np.zeros((width, height, 4))
+
+#粒子数リスト
+particles = np.ones((width, height)) * initialvalue
 
 #位置ベクトル計算
 def calc_pos_vector(i, j):
@@ -70,9 +76,12 @@ def calc_dotprod(pos_x_list,pos_y_list,u_x,u_y):
 def clac_speed_sys(dot_product_list,pos_x_list,pos_y_list):
     for i in range(width):
         for j in range(height):
-            #1stepを考えて0.0002倍
-            speed_sys_x_list[i,j] =  100 * pos_x_list[i,j] / dot_product_list[i,j]  * 0.002
-            speed_sys_y_list[i,j] =  100 * pos_y_list[i,j] / dot_product_list[i,j]  * 0.002
+            if dot_product_list[i,j] !=0:
+                speed_sys_x_list[i,j] =  1000 * pos_x_list[i,j] / dot_product_list[i,j]
+                speed_sys_y_list[i,j] =  1000 * pos_y_list[i,j] / dot_product_list[i,j]
+            else:
+                speed_sys_x_list[i,j] = u_y
+                speed_sys_y_list[i,j] = u_x
     return speed_sys_x_list, speed_sys_y_list
 
 #拡散シミュレーション
@@ -82,30 +91,42 @@ def maxwell_boltzmann_2d(N, T, M, speed_sys_x, speed_sys_y):
     sigma = np.sqrt(k_B * T / mass)
     
     # x方向とy方向の速度成分を生成
-    vx = np.random.normal(0, sigma, N) + speed_sys_x  
-    vy = np.random.normal(0, sigma, N) + speed_sys_y 
+    vx = np.random.normal(0, sigma, N) + speed_sys_x
+    vy = np.random.normal(0, sigma, N) + speed_sys_y
 
     #simulationを考えて時間dt辺りの移動量を推定
     vx = vx * 0.0002
     vy = vy * 0.0002
     
-    return vx, vy
-
-def classify_particles(vx, vy):
-    counts = {"+x": 0, "+y": 0, "-x": 0, "-y": 0}
+    counts = np.zeros(4)
     
     for x, y in zip(vx, vy):
         angle = np.arctan2(y, x) * 180 / np.pi
         if -45 <= angle < 45:
-            counts["+x"] += 1
+            counts[0] += 1
         elif 45 <= angle < 135:
-            counts["+y"] += 1
+            counts[1] += 1
         elif 135 <= angle <= 180 or -180 <= angle < -135:
-            counts["-x"] += 1
+            counts[2] += 1
         elif -135 <= angle < -45:
-            counts["-y"] += 1
+            counts[3] += 1
 
     return counts
+
+#シミュ本番
+def mesh_simulation(spread, particles):
+    particles_new = particles.copy()
+    
+
+    for i in range(1,width-1):
+        for j in range(1,height-1):
+            if i != width and j != height:
+                particles_new[i,j] = particles[i-1,j] * (spread[i-1,j,3]/N) +\
+                    particles[i+1,j] * (spread[i+1,j,1]/N) +\
+                    particles[i,j-1] * (spread[i,j-1,0]/N) +\
+                          particles[i,j+1] * (spread[i,j-1,2]/N)
+    
+    return particles_new
 
 # 位置ベクトルリストの作成
 for i in range(width):
@@ -122,15 +143,49 @@ dot_product_list =  calc_dotprod(pos_x_list,pos_y_list,u_x,u_y)
 speed_sys_x_list, speed_sys_y_list = clac_speed_sys(dot_product_list,pos_x_list,pos_y_list)
 
 #拡散シミュ計算
+for i in range(width):
+    for j in range(height):
+        spread[i,j] = maxwell_boltzmann_2d(N, T, M, speed_sys_x_list[i,j], speed_sys_y_list[i,j])
+    print(i)
+
+#拡散分布の出力
+# csvファイルのパス 
+csv_file_path = 'maxwell_boltzmann.csv' 
+
+# CSVファイルに出力する 
+with open(csv_file_path, mode='w', newline='') as file: 
+    writer = csv.writer(file) 
+    # 各要素を行に追加 
+    for a in range(i): 
+        for b in range(j): 
+            writer.writerow(spread[a][b])
+
+#mesh拡散のシミュレーション
+for i in range(time_steps):
+    particles = mesh_simulation(spread, particles)
+    print(i)
+
+particles = particles.T
+
+#結果のプロット
+
+fig, ax = plt.subplots()
+cax = ax.imshow(particles, cmap='plasma', interpolation='nearest')
+plt.title(f'Step {i * 50} Particles Distribution')
+plt.xlabel('X-axis (mm)')
+plt.ylabel('Y-axis (mm)')
+plt.grid(True)
+fig.colorbar(cax, ax=ax, label='particles', orientation='vertical')
+plt.show()
 
 # 矢印をプロット
-plt.figure(figsize=(10, 10))
-plt.quiver(Y_mesh, X_mesh, speed_sys_x_list, speed_sys_y_list, angles='xy', scale_units='xy', scale=1)
-plt.title("system_speed_list")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.xlim(0,101)
-plt.ylim(0,101)
-plt.grid()
-plt.show()
+# plt.figure(figsize=(10, 10))
+# plt.quiver(Y_mesh, X_mesh, speed_sys_x_list, speed_sys_y_list, angles='xy', scale_units='xy', scale=1)
+# plt.title("system_speed_list")
+# plt.xlabel("x")
+# plt.ylabel("y")
+# plt.xlim(0,101)
+# plt.ylim(0,101)
+# plt.grid()
+# plt.show()
 
